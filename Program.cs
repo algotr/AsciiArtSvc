f056d71b66779d8c2308a0d456adc80145fa17d8
Ali Salim <asg505@hotmail.com>
2024-10-01 11:31:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+using Figgle;
+using AsciiArtSvc;
+using BarcodeStandard;
+using System.Drawing;
+using System.Drawing.Imaging;
+using SkiaSharp;
+
+const StringComparison SCIC = StringComparison.OrdinalIgnoreCase;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", (
+    int? skip,
+    int? take,
+    FiggleTextDirection? dir,
+    string? name,
+    string? order) =>
+{
+    var query = from f in AsciiArt.AllFonts.Value
+                where
+                (name == null || f.Name.Contains(name, SCIC))
+                && (dir == null || f.Font.Direction == dir)
+                select f;
+    if (string.Equals("desc", order, SCIC))
+    {
+        query = query.OrderByDescending(f => f.Name);
+    }
+    else
+    {
+        query = query.OrderBy(f => f.Name);
+    }
+
+    return query
+    .Skip(skip ?? 0)
+    .Take(take ?? 200)
+    .Select(f => f.Name);
+});
+
+app.MapGet("/{text}",
+    (string text, string? font) =>
+    AsciiArt.Write(text, out var asciiText, font)
+    ? Results.Content(asciiText!)
+    : Results.NotFound());
+
+
+app.MapGet("/barcode/{text}", (string text) => { 
+    var barcode = new Barcode();
+    barcode.ImageFormat = SkiaSharp.SKEncodedImageFormat.Jpeg;
+    _ = barcode.Encode(
+        BarcodeStandard.Type.Code128,
+        text,
+        SKColors.Black, 
+        SKColors.White);
+    return Results.File(
+        barcode.EncodedImageBytes,
+        "image/jpeg");
+});
+
+app.Run();
